aac2244b1a2dc74c196bbd56b420846f7c4d8e7c
SatishKodali <satish.kodali@aa.com>
2019-07-08 19:48:25 -0500

    works for linuxjenkins

--- undeploy.sh
+++ undeploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-deployment_info=$(curl -H "Authorization: Basic <base64 username:password>" "https://api.enterprise.apigee.com/v1/organizations/o/apis/api-name/deployments") 
+AUTH_PASS="$1:$2"
+deployment_info=$(curl -u $AUTH_PASS "https://api.enterprise.apigee.com/v1/organizations/$3/apis/cicd-api/deployments") 
 
 rev_num=$(jq -r .environment[0].revision[0].name <<< "${deployment_info}" ) 
 env_name=$(jq -r .environment[0].name <<< "${deployment_info}" ) 
@@ -15,8 +16,8 @@ echo $org_name
 echo $env_name
 echo $pre_rev
 
-curl -X DELETE --header "Authorization: Basic <base64 username:password>" "https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$rev_num/deployments"
+curl -X DELETE -u $AUTH_PASS "https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$rev_num/deployments"
 
-curl -X DELETE --header "Authorization: Basic <base64 username:password>" "https://api.enterprise.apigee.com/v1/organizations/$org_name/apis/$api_name/revisions/$rev_num"
+curl -X DELETE -u $AUTH_PASS "https://api.enterprise.apigee.com/v1/organizations/$org_name/apis/$api_name/revisions/$rev_num"
 
-curl -X POST --header "Content-Type: application/x-www-form-urlencoded" --header "Authorization: Basic <base64 username:password>" "https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$pre_rev/deployments"
+curl -X POST --header "Content-Type: application/x-www-form-urlencoded" -u $AUTH_PASS "https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$pre_rev/deployments"
