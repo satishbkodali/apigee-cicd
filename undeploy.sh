4abe3c777ef5393abe4793f93f3d79baffe1033e
SatishKodali <satish.kodali@aa.com>
2019-07-10 10:14:35 -0500

    works for linuxJenkins

--- undeploy.sh
+++ undeploy.sh
@@ -10,14 +10,25 @@ org_name=$(jq -r .organization <<< "${deployment_info}" )
 declare -r num1=1
 pre_rev=$(expr "$rev_num" - "$num1")
 
-echo $rev_num
-echo $api_name
-echo $org_name
-echo $env_name
-echo $pre_rev
+echo currRev=$rev_num
+echo apiName=$api_name
+echo orgName=$org_name
+echo envName=$env_name
+echo prevRev=$pre_rev
 
-curl -X DELETE -u $AUTH_PASS "https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$rev_num/deployments"
+undeployRevUrl="https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$rev_num/deployments"
+delRevUrl="https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$rev_num/deployments"
+deployPrevRevUrl="https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$pre_rev/deployments"
 
-curl -X DELETE -u $AUTH_PASS "https://api.enterprise.apigee.com/v1/organizations/$org_name/apis/$api_name/revisions/$rev_num"
+echo "undeploying recently deloyed revision $rev_num"
+echo "DELETE call $undeployRevUrl"
 
-curl -X POST --header "Content-Type: application/x-www-form-urlencoded" -u $AUTH_PASS "https://api.enterprise.apigee.com/v1/organizations/$org_name/environments/$env_name/apis/$api_name/revisions/$pre_rev/deployments"
+curl -X DELETE -u $AUTH_PASS $undeployRevUrl
+
+echo "Deleting recently deloyed revision $rev_num"
+echo "DELETE call $delRevUrl"
+curl -X DELETE -u $AUTH_PASS $delRevUrl
+
+echo "Deploying previous revision $prev_rev again"
+echo "POST call $deployPrevRevUrl"
+curl -X POST --header "Content-Type: application/x-www-form-urlencoded" -u $AUTH_PASS $deployPrevRevUrl
